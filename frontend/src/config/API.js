63ea7a9fbc2c1ef1eebed80571280119adf9d77e
Héctor Manuel Muñoz Flores <skyspartanblack@gmail.com>
2022-02-17 23:58:55 -0600

    changes downloaded

--- frontend/src/config/API.js
+++ frontend/src/config/API.js
@@ -1,7 +1,7 @@
 //Dev
-//const baseUrl = "http://localhost:4000/api";
+const baseUrl = "http://localhost:4000/api";
 
 //production
-const baseUrl = "http://192.168.1.77:4000/api";
+//const baseUrl = "http://192.168.1.77:4000/api";
 
 export default baseUrl;
