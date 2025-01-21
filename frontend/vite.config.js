eeefac8817eb384b5cdf2d7cf921aef689a25bda
hector.munoz <hectorm.flores09@gmail.com>
2025-01-21 15:23:53 -0600

    chore: removed output dir

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,8 +3,5 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  build: {
-    outDir: 'public' // Change this to your desired folder name
-  }
+  plugins: [react()]
 })
